e4830f46650836300f5cf8bba84efcf6aec8780c
julien <kalaycijulien@gmail.com>
2024-04-22 21:56:21 +0200

    init projet

---
+++ src/main/java/com/jk/gestiontrainserveur/GestionTrainServeurApplication.java
@@ -0,0 +1,13 @@
+package com.jk.gestiontrainserveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTrainServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTrainServeurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/gestiontrainserveur/GestionTrainServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.gestiontrainserveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTrainServeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
